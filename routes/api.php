69cb8a64485b92fede5a8b42a8f041f12ec5559e
Maikol98 <56239415+Maikol98@users.noreply.github.com>
2020-10-12 22:22:30 -0400

    commit5

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', 'userController@login');
 
 //--------------------CATEGORIA----------------
 Route::apiResource('Categoria','CategoriaController'); //-
